f370a707c0ba07b17c9152f7f2f72a2f56379269
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2023-02-10 11:15:01 +0100

    boton

---
+++ proyecto pricipal.py
@@ -0,0 +1,19 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Infovip"
+    
+    
+        
+    t =ft.Text(value="INFOVIP", color="#6F82EB",size=30)
+    page.add(t)
+    
+    botonRegistrar =ft.ElevatedButton(text="Registrar")
+    page.add(botonRegistrar)
+    
+
+
+
+
+
+ft.app(target=main)
