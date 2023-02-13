9de940ba22ecd4c11733f53900518484c8c4a2b0
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2023-02-13 10:39:48 +0100

    Juntando ramas icono y botón registrar

--- proyecto pricipal.py
+++ proyecto pricipal.py
@@ -7,7 +7,7 @@ def main(page: ft.Page):
     page.add(img)
     t =ft.Text(value="INFOVIP", color="#6F82EB",size=30)
     page.add(t)
-    botonRegistrar =ft.ElevatedButton(text="Reg  istrar")
+    botonRegistrar =ft.ElevatedButton(text="Registrar")
     page.add(botonRegistrar)
     
-ft.app(target=main)
+ft.app(target=main, assets_dir="Imagen")
