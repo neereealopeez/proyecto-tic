2cf56bdc9b0c92be826b1b9c3dc110da4b3cd540
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2023-02-13 11:14:16 +0100

    Botón usuario y contraseña

--- proyecto pricipal.py
+++ proyecto pricipal.py
@@ -5,9 +5,15 @@ def main(page: ft.Page):
     img = ft.Image(src=f"esto no se ve", width=300, height=300)
     img.src = "imagen.png"
     page.add(img)
-    t =ft.Text(value="INFOVIP", color="#6F82EB",size=30)
+    t =ft.Text(value="INFOVIP", color="#6F82EB",size=30, font_family="Times New Roman")
     page.add(t)
+    textField_Usuario= ft.TextField(label="Usuario", hint_text="Escribe aquí tu nombre de usuario", width=400)
+    textField_Contraseña= ft.TextField(label="Contraseña", hint_text="Escribe aquí tu contraseña", width=400)
     botonRegistrar =ft.ElevatedButton(text="Registrar")
-    page.add(botonRegistrar)
+    page.add(textField_Usuario, textField_Contraseña, botonRegistrar)
     
 ft.app(target=main, assets_dir="Imagen")
+
+
+
+
