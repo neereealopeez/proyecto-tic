a4cc3a43544f0b0103e935efa0ab3d8d1e0f8b09
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2023-02-14 14:04:52 +0100

    restauracion 2

--- proyecto pricipal.py
+++ proyecto pricipal.py
@@ -9,13 +9,15 @@ def main(page: ft.Page):
     img = ft.Image(src=f"esto no se ve", width=300, height=300)
     img.src = "imagen.png"
     page.add(img)
+   
     t =ft.Text(value="INFOVIP", color="#6F82EB",size=30, font_family="Times New Roman")
     page.add(t)
+
     botonRegistrar =ft.ElevatedButton(text="Registrar", icon="Login")
-    page.add(textField_Usuario, textField_Contraseña, botonRegistrar)
     textField_Contraseña= ft.TextField(label="Contraseña", hint_text="Escribe aquí tu contraseña", width=400)
     textField_Usuario= ft.TextField(label="Usuario", hint_text="Escribe aquí tu nombre de usuario", width=400)
     
+    page.add(textField_Usuario, textField_Contraseña, botonRegistrar)
 
 
 
