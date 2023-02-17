600ae591de4cadbfd4f133158b45bfd1fb7bfee7
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2023-02-17 11:11:58 +0100

    Intentos

--- proyecto pricipal.py
+++ proyecto pricipal.py
@@ -3,24 +3,12 @@ import flet as ft
 def main(page: ft.Page):
   
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-    def comprobar_login(e):
-    a.value = f"Textboxes values are:  '{textField_Contraseña.value}', '{textField_Usuario.value}'"
-    page.update()
-
     page.title="InfoVIP"
     page.bgcolor="#DDE3FF"
+    
     img = ft.Image(src=f"esto no se ve", width=200, height=200)
     img.src = "imagen2.png"
-    page.add(img)
-    t =ft.Text(value="INFOVIP", color="#6F82EB",size=30, font_family="Times New Roman")
-    page.add(t)
-    a = ft.Text()
-    textField_Usuario= ft.TextField(label="Usuario", hint_text="Escribe aquí tu nombre de usuario", width=400)
-    textField_Contraseña= ft.TextField(label="Contraseña", hint_text="Escribe aquí tu contraseña", width=400)
-    botonRegistrar =ft.ElevatedButton(text="Registrar", icon="Login",on_click=comprobar_login)
-    page.add(textField_Usuario, textField_Contraseña, botonRegistrar)
-
+   
 
     def cargarFichero():
         vDatos = []
@@ -30,6 +18,36 @@ def main(page: ft.Page):
         
         f.close()
         return vDatos
+
+    def comprobar_login(e):
+        usuario = textField_Usuario.value
+        password = textField_Contraseña.value
+        vUsus = cargarFichero()
+
+        if (vUsus.count(usuario) > 0) and (vUsus.count(password) > 0):
+            print ("usuario en lista")
+
+        else:
+            dlg= ft.AlertDialog(title=ft.Text(f"Usuario y/o contraseña incorrectos. Te quedan {textField_Intentos.value} intentos."))
+            textField_Intentos.value = textField_Intentos.value-1
+            page.dialog = dlg
+            dlg.open = True
+            if 
+
+            print(textField_Intentos.value)
+        
+        page.update()
+
+
+    texto= ft.Text()
+    t =ft.Text(value="INFOVIP", color="#6F82EB",size=30, font_family="Times New Roman")
+    textField_Usuario= ft.TextField(label="Usuario", hint_text="Escribe aquí tu nombre de usuario", width=400)
+    textField_Contraseña= ft.TextField(label="Contraseña", hint_text="Escribe aquí tu contraseña", width=400)
+    textField_Intentos= ft.TextField(value=3)
+    botonRegistrar =ft.ElevatedButton(text="Registrar", icon="Login",on_click=comprobar_login)
+    page.add(textField_Usuario, textField_Contraseña, botonRegistrar, t, img, texto)
+
+    
 
 
 
