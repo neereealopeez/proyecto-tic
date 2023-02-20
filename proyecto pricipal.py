4bef692639b4603088465d648dcb7b1bf62b54ed
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2023-02-20 11:10:36 +0100

    si

--- proyecto pricipal.py
+++ proyecto pricipal.py
@@ -1,13 +1,15 @@
 import flet as ft
 
 def main(page: ft.Page):
-  
+    
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    img = ft.Image(src=f"esto no se ve", width=200, height=200)
+    img.src = "imagen2.png"
+   
     page.title="InfoVIP"
     page.bgcolor="#DDE3FF"
     
-    img = ft.Image(src=f"esto no se ve", width=200, height=200)
-    img.src = "imagen2.png"
    
 
     def cargarFichero():
@@ -23,16 +25,20 @@ def main(page: ft.Page):
         usuario = textField_Usuario.value
         password = textField_Contraseña.value
         vUsus = cargarFichero()
-
+        print (usuario)
+        print (vUsus)
         if (vUsus.count(usuario) > 0) and (vUsus.count(password) > 0):
             print ("usuario en lista")
+            page.clean()
 
         else:
             dlg= ft.AlertDialog(title=ft.Text(f"Usuario y/o contraseña incorrectos. Te quedan {textField_Intentos.value} intentos."))
             textField_Intentos.value = textField_Intentos.value-1
             page.dialog = dlg
             dlg.open = True
-            if 
+
+            if textField_Intentos.value==-1:
+                page.window_close()
 
             print(textField_Intentos.value)
         
@@ -40,12 +46,12 @@ def main(page: ft.Page):
 
 
     texto= ft.Text()
-    t =ft.Text(value="INFOVIP", color="#6F82EB",size=30, font_family="Times New Roman")
     textField_Usuario= ft.TextField(label="Usuario", hint_text="Escribe aquí tu nombre de usuario", width=400)
     textField_Contraseña= ft.TextField(label="Contraseña", hint_text="Escribe aquí tu contraseña", width=400)
     textField_Intentos= ft.TextField(value=3)
-    botonRegistrar =ft.ElevatedButton(text="Registrar", icon="Login",on_click=comprobar_login)
-    page.add(textField_Usuario, textField_Contraseña, botonRegistrar, t, img, texto)
+    botonRegistrar =ft.FilledButton(text="Registrar", icon="Login",on_click=comprobar_login)
+    botonRegistrar.bgcolor="#0F0648"
+    page.add(img,textField_Usuario, textField_Contraseña, botonRegistrar, texto)
 
     
 
